72144a2185d09369ed3f4df43cda4819cf281985
Patryk Karwowski <p-karwowski-dev@gmail.com>
2024-10-18 15:23:52 +0100

    added updateScore expectation

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -11,4 +11,10 @@ describe('Scoreboard', () => {
         scoreboard.startMatch('Poland', 'Brazil');
         expect(scoreboard.getSummary()).toEqual(['Poland 0 - Brazil 0']);
     });
+
+    test('Allow update the score of a match:', () => {
+        scoreboard.startMatch('Poland', 'Brazil');
+        scoreboard.updateScore(1, 0);
+        expect(scoreboard.getSummary()).toEqual(['Poland 3 - Brazil 0']);
+    });
 });
